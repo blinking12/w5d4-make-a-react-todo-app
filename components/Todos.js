c554ba3fb20324443420848bb0e1f42b3e0fefb3
Troy Denney <tvdenney@gmail.com>
2016-10-28 16:13:52 -0400

    adding comments

--- components/Todos.js
+++ components/Todos.js
@@ -8,11 +8,9 @@ const Todos = (props) => {
     })
     return <div>
         {TodoItems}
-    </div>
+    </div>  //Here is where the TodoItems component is returned to me div
 }
-
+//This is where Todos gets exported
 export default Todos
 
-    // Create a stateless component named Todos that takes props.todos and maps it to a new array of TodoItem components called var items, each receiving a prop named item set to the current item in the map. Remember to put key={i} onto each component that corresponds to the current map index. You'll do this in the render() method.
-
-    // In that same render method, return the new items variable that the map function made. Make sure you use JSX tags around the items variable.
+    
--- js/global.js
+++ js/global.js
@@ -2,38 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Todos from '../components/Todos'
 
-var todos = []
-console.log(todos)
+var todos = []  //this is a global variable
+console.log(todos)  //This is where I'm logging the global variable to make sure it works
 
+// Here is where I added keypress event listener, using an id.
 document.getElementById('textBox').addEventListener('keypress', enter);
 
+//Here is where I added click event listener, using an id
 document.getElementById('addBtn').addEventListener('click', addTodo);
 
 var addTodo = function() {
     // 1. get todo input field
     var todoItem = document.getElementById('textBox').value
 
-    // 2. push todoItem onto todos array
+    // 2. push todoItem onto todos array, pushes to the end.
     todos.push(todoItem)
 
     // 3. clear out textBox value
     document.getElementById('textBox').value = ''
 
-    // 4. call renderView(todos)
+    // 4. call renderView(todos) and pass in the array, render as a visual form.
     renderView(todos)
 }
-// renderView(todos)
 
+
+//this is how I made the enter key work on the input
 function enter(event) {
     if (event.key === 'Enter') {
         addTodo()
     }
 }
-
+//This is my render function, it will add items out to my page.
 var renderView = (data) => {
     console.log(data)
-    ReactDOM.render(
+    ReactDOM.render(  //relates to the import up top
         <Todos todos={data} />,//render can return or mount only 1 component
-        document.getElementById('todos') //<=This has to be an ID
+        document.getElementById('todos') //<=This is where I will mount the data
     )
 }
